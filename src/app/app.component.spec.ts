a705f5bd13f867cd246baa0b5c69c629846615b9
WEI JIN <swissjw@gmail.com>
2023-10-03 14:26:54 -0400

    Changed to eComm-app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Shopping-app'`, () => {
+  it(`should have as title 'eComm-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Shopping-app');
+    expect(app.title).toEqual('eComm-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Shopping-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('eComm-app app is running!');
   });
 });
